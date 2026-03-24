9f77dd183b53d605310d2b20c70d8717dd5610a7
Ibai Lopez <ilopez267@ikasle.ehu.eus>
2026-03-24 15:38:57 +0100

    Experiment implementation + prompt usage changed

--- template.py
+++ template.py
@@ -3,11 +3,13 @@ Import every library needed for the algorithm to be designed before
 starting to implement the code. The imports must be added in the
 specified field.
 """
-#Imports start
 
-#Imports end
 
 def main(instance):
+    #Imports start
+
+    #Imports end
+
     solution = None
     """
     ·Every helper function must be implemented inside main() function.
@@ -15,6 +17,7 @@ def main(instance):
      a permutation for the given LOP instance.
     ·The only thing to change is the TODO part, the rest should remain the same.
      Including parameters and return.
+    ·The input parameter "instance" is a np.array of size nxn, n being the length of the solution.
     """
     #TODO: implement the algorithm here.
 
